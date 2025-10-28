377c818d80f558bb5e5707b54e395c6599fa5c40
gomes-milindu <gomesmilindu@gmail.com>
2025-10-28 10:09:48 +0530

    check github

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+app.listen(5000, success)
+
+function success() {
+    console.log("Server is running on port 5000");
+}
+
+console.log("Hello, ES Modules!");
